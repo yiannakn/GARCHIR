06aa7edf069eac77d6f7b68036e4210a28860073
yiannakn <nikolas254@gmail.com>
2016-08-14 23:08:24 +0300

    changes

--- BreakPointDet/BreakPointDet.R
+++ BreakPointDet/BreakPointDet.R
@@ -22,14 +22,28 @@
 rm(list = ls(all = TRUE))
 graphics.off()
 
-## Install and load packages
-libraries = c("changepoint", "zoo")
-lapply(libraries, function(x) if (!(x %in% installed.packages( ))) {
+## Intall and load packages
+libraries = c("tseries", "stockPortfolio", "zoo", "changepoint")
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
   install.packages(x)
 })
 lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 
-## Determine the breakpoints
+## Get Data and Returns and Returns demeaned
+SPL     = read.csv("DAX Data Comma.csv")
+SPL.zoo = zoo(SPL[, -1], 
+              order.by = as.Date(strptime(as.character(SPL[, 1]), "%Y-%m-%d")))
+prices  = SPL.zoo$Adj.Close
+returns = diff(log(prices))
+mean       = mean(returns)
+returns.dm = returns - mean
+
+int.rate      = read.csv("int_rate.csv", header = T, sep = ",")
+int.rate.zoo  = zoo(int.rate[, -1], 
+                    order.by = as.Date(strptime(as.character(int.rate[, 1]), 
+                                                "%Y-%m-%d")))
+
+## Breakpoint Testing
 breakpoints     = cpt.var(returns, penalty = "MBIC", pen.value = 0.05, 
                           know.mean = FALSE, method = "PELT")
 breakpoints.vec = breakpoints@cpts
